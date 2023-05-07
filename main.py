fe276248d17fdfd636beb83d4cefe0115bf7dd5c
AllCirclesPresuppose <114363778+AllCirclesPresuppose@users.noreply.github.com>
2023-05-07 22:10:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#myName = input("What's your name? ")
+#myLunch = input("What are you having for lunch? ")
+#print(myName, "is going to be chowing down on", myLunch, "very soon!")
+#yourName = input("Name: ")
+#whatYear = input("What year is it?: ")
+#print(yourName, "thinks it is", whatYear)
+
+#yourName = input("Name: ")
+#whatYear = input("What year is it?: ")
+#print(yourName, "thinks it is", whatYear)
+
+#print("=== Your Song Generator ===")
+#print("""You'll be asked a bunch of questions
+#then we'll make you up an amazing
+#song, totally copyright free 😭""")
+#print()
+#person = input("Name a person famous for something good: ")
+#thing = input("Name a thing they did: ")
+#place = input("Name a place you like: ")
+#rhyme = input("Give me a verb that rhymes with your #person's name: ")
+#print()
+#print("There was a person called", person)
+#print("Who did something cool like", thing, "at the #wonderful", place, "where you'll find me", rhyme)
+
+food = input("Name a food: ")
+plant = input("Name a type of plant: ")
+cookStyle = input("Name a method of cooking: ")
+burned = input("What is a word that describes burnt food? ")
+diy = input("Name a DIY item: ")
+
+print()
+print("MENU")
+print(cookStyle, " ", food, " with ", burned, " ", plant, " on a bed of ", diy)
